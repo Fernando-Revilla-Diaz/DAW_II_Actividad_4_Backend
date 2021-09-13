58cbf5a1b107d8305ed28ca759532f5bbacd26e5
Fernando Revilla Díaz <i201910202@cibertec.edu.pe>
2021-09-12 20:13:34 -0500

    Commit inicial del proyecto

---
+++ src/main/java/com/empresa/DawIiActividad4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawIiActividad4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawIiActividad4BackendApplication.class, args);
+	}
+
+}
