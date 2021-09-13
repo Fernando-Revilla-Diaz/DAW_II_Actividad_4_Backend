b14587c9c8f7b1109d87b1a862e046d686792372
Fernando Revilla Díaz <i201910202@cibertec.edu.pe>
2021-09-13 18:58:34 -0500

    Servicios REST para listado y registro de medicamentos listo.

--- src/main/java/com/empresa/controllers/MedicamentoController.java
+++ src/main/java/com/empresa/controllers/MedicamentoController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import com.empresa.utils.Constants;
 
 @Controller
 @RequestMapping("/rest/medicamento")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MedicamentoController {
 
 	@Autowired
